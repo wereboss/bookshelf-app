587fda65ec38328b0fe763e7ac3fdd2963a89e49
Srinath Menon <tellsrinath@gmail.com>
2021-12-19 18:40:12 +0800

    initial working copy

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+// bookshelf-app/src/index.tsx
+
+// Import deps
+import React from "react";
+import { render } from "react-dom";
+
+// Import components
+import { Bookshelf } from "./components/bookshelf";
+
+// Import styles
+import "./styles/styles.css";
+
+// Find div container
+const rootElement = document.getElementById("root");
+
+// Render Bookshelf component in the DOM
+render(<Bookshelf />, rootElement);
